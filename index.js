18d2a28c155e97cb21e016e8027aed039e6faa53
Razem <info@razem.cz>
2025-02-09 20:33:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let hashData = new URLSearchParams(window.location.hash.slice(1))
+let contentInput = document.querySelector('#content')
+
+// Set initial content from hash
+contentInput.value = hashData.get('x')
+
+// Update hash on input
+contentInput.addEventListener('input', () => {
+  hashData.set('x', contentInput.value)
+  window.location.hash = '#' + hashData
+})
+
+// Set focus on page load
+contentInput.focus()
+
+// Invoke share action
+document.querySelector('#share').addEventListener('click', () => {
+  navigator.share?.({ url: window.location.href })
+})
