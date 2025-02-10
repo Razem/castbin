cfced34809e208c02e7ba6439ff8b14c141c0f89
Razem <info@razem.cz>
2025-02-10 09:45:24 +0100

    Add current URL to the share link

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let hashData = new URLSearchParams(window.location.hash.slice(1))
 let contentInput = document.querySelector('#content')
+let shareLink = document.querySelector('#share')
 
-// Set initial content from hash
+// Set initial content from URL
 contentInput.value = hashData.get('x')
+shareLink.href = window.location.href
 
-// Update hash on input
+// Update URL on input
 contentInput.addEventListener('input', () => {
   hashData.set('x', contentInput.value)
   window.location.hash = '#' + hashData
+  shareLink.href = window.location.href
 })
 
 // Invoke share action
-document.querySelector('#share').addEventListener('click', () => {
+shareLink.addEventListener('click', (e) => {
+  e.preventDefault()
   navigator.share?.({ url: window.location.href })
 })
